daab7ee047bce959d10d37a0b98aa7ecb0702544
Sarif200 <firas.vandevelde@hotmail.com>
2025-11-20 17:07:13 +0100

    also export dpth as a webcam

--- examples/kinect_webcam/Kinect_Webcam.cpp
+++ examples/kinect_webcam/Kinect_Webcam.cpp
@@ -22,7 +22,7 @@ void sigint_handler(int s)
   protonect_shutdown = true;
 }
 
-bool init_v4l2_device(const char *device, int width, int height,int* stream)
+bool init_v4l2_device(const char *device, int width, int height,int* stream,int pixformat)
 {
   *stream = open(device, O_WRONLY);
   if (*stream < 0)
@@ -36,7 +36,7 @@ bool init_v4l2_device(const char *device, int width, int height,int* stream)
   v4l2_fmt.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
   v4l2_fmt.fmt.pix.width = width;
   v4l2_fmt.fmt.pix.height = height;
-  v4l2_fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_BGR24;
+  v4l2_fmt.fmt.pix.pixelformat = pixformat;//V4L2_PIX_FMT_BGR24;
   v4l2_fmt.fmt.pix.sizeimage = width * height * 3;
   v4l2_fmt.fmt.pix.field = V4L2_FIELD_NONE;
 
@@ -50,32 +50,44 @@ bool init_v4l2_device(const char *device, int width, int height,int* stream)
   return true;
 }
 
-void write_frame_to_v4l2(unsigned char *rgb_data, int width, int height)
+void write_frame_to_v4l2(unsigned char *rgb_data, int width, int height,int* stream)
 {
-  if (v4l2_color < 0)
+  if (*stream < 0)
     return;
 
   cv::Mat frame(height, width, CV_8UC4, rgb_data);
   cv::Mat bgr_frame;
   cv::cvtColor(frame, bgr_frame, cv::COLOR_BGRA2BGR);
 
-  write(v4l2_color, bgr_frame.data, width * height * 3);
+  write(*stream, bgr_frame.data, width * height * 3);
 }
 
-int main(int argc, char *argv[])
+void write_frame_to_depth(unsigned char *rgb_data, int width, int height,int* stream)
+{
+  if (*stream < 0)
+    return;
+
+  cv::Mat frame(height, width, CV_32SC1, rgb_data); //CV_16UC1
+  cv::Mat bgr_frame;
+  cv::cvtColor(frame, bgr_frame, cv::COLOR_GRAY2BGR );
+
+  write(*stream, bgr_frame.data, width * height * 3);
+}
+
+int main(int argc, char *argv[])frame.data
 {
   libfreenect2::Freenect2 freenect2;
   libfreenect2::Freenect2Device *dev = nullptr;
   libfreenect2::PacketPipeline *pipeline = nullptr;
 
   
-  if (!init_v4l2_device("/dev/video10", 1920, 1080,&v4l2_color) < 0)
+  if (!init_v4l2_device("/dev/video10", 1920, 1080,&v4l2_color,V4L2_PIX_FMT_BGR24) < 0)
   {
     std::cerr << "Failed to initialize v4l2loopback device" << std::endl;
     return -1;
   }
 
-  if (!init_v4l2_device("/dev/video10", 1920, 1080,&v4l2_depth) < 0)
+  if (!init_v4l2_device("/dev/video11", 512, 424,&v4l2_depth,V4L2_PIX_FMT_BGR24) < 0) //V4L2_PIX_FMT_Y16
   {
     std::cerr << "Failed to initialize v4l2loopback-depth device" << std::endl;
     return -1;
@@ -98,11 +110,17 @@ int main(int argc, char *argv[])
   }
 
   signal(SIGINT, sigint_handler);
-  
-  libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color);
+   int types = libfreenect2::Frame::Color | libfreenect2::Frame::Depth | libfreenect2::Frame::Ir;
+  libfreenect2::SyncMultiFrameListener listener(types);
   libfreenect2::FrameMap frames;
 
   dev->setColorFrameListener(&listener);
+  dev->setIrAndDepthFrameListener(&listener);
+
+  /// [registration setup]
+  libfreenect2::Registration* registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+  libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
+  /// [registration setup]
 
   if (!dev->start())
     return -1;
@@ -118,8 +136,17 @@ int main(int argc, char *argv[])
       return -1;
     }
     libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
 
-    write_frame_to_v4l2(rgb->data, rgb->width, rgb->height);
+    /// [registration]
+      registration->apply(rgb, depth, &undistorted, &registered);
+    /// [registration]
+    
+    write_frame_to_v4l2(rgb->data, rgb->width, rgb->height,&v4l2_color);
+    write_frame_to_v4l2(depth->data, depth->width, depth->height,&v4l2_depth);
+
+
+
 
     listener.release(frames);
   }
@@ -127,5 +154,6 @@ int main(int argc, char *argv[])
   dev->stop();
   dev->close();
   close(v4l2_color);
+  close(v4l2_depth);
   return 0;
 }
